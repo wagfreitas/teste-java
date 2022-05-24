7dea20ccfff2ad857fbe5b1c879509026b35e964
wagner alves <wagfreitas@hotmail.com>
2022-05-23 21:12:24 -0300

    first commit

--- src/br/com/dio/Java.java
+++ src/br/com/dio/Java.java
@@ -6,8 +6,5 @@ public class Java {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
-//        int a = 5;
-//        int b = 3;
-//        System.out.println("Hello World " + (a+b));
     }
 }
